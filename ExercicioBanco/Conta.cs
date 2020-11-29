8f2f4f999c36c68e37aafdd31f1a2ea9766b32b0
Emerson Trevisan <54911322+trevisanemerson@users.noreply.github.com>
2020-11-29 18:39:46 -0300

    Primeiro exercicio de banco

---
+++ ExercicioBanco/Conta.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioBanco
+{
+    class Conta
+    {
+        public int NumeroConta { get; set; }
+        public string Titular { get; set; }
+        public double Saldo { get; set; }
+
+        public Conta(int numero, string titular, double saldo) {
+            NumeroConta = numero;
+            Titular = titular;
+            Saldo = saldo;
+        }
+        public Conta()
+        {
+        }
+
+        public double AdicionarSaldo(double quantidade) {
+            Saldo += quantidade;
+            return Saldo;            
+        }
+        public double RetirarSaldo(double quantidade) {
+            double taxa = 5.00;
+            Saldo -= quantidade;
+            Saldo -= taxa;
+            return Saldo;
+        }
+        public override string ToString()
+        {
+            return "Conta " + NumeroConta
+            + " , Titular: " + Titular
+            + " , Saldo: " + Saldo.ToString("F2");
+        }
+    }
+}
---
+++ ExercicioBanco/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioBanco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char inicial;
+            double quantidade;
+
+            Conta c = new Conta();
+
+            Console.Write("Entre o numero da conta: ");
+            c.NumeroConta = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre com o titular da conta: ");
+            c.Titular = Console.ReadLine();
+
+            Console.Write("Haverá primeiro depósito? (s/n): ");
+            inicial = char.Parse(Console.ReadLine());
+
+            if (inicial == 's') {
+                Console.Write("Digite o valor do dep. inicial: ");
+                quantidade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                c.AdicionarSaldo(quantidade);
+            }
+
+            Console.Write("Dados da Conta: " + c);
+            
+            Console.Write("Entre um valor para depósito: ");
+            quantidade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            c.AdicionarSaldo(quantidade);
+
+            Console.WriteLine("Dados atualizados: " + c);
+
+            Console.Write("Entre um valor para o saque: ");
+            quantidade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            c.RetirarSaldo(quantidade);
+
+            Console.WriteLine("Dados atualizados: " + c);
+
+
+
+
+        }
+    }
+}
